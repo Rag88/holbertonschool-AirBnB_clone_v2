7a09b8be2b0bec758d073407f3afde4c4e74d319
21Kgold <claudiarpa@gmail.com>
2022-08-11 21:01:32 -0700

    fix Review class

--- models/review.py
+++ models/review.py
@@ -6,7 +6,7 @@ from sqlalchemy import ForeignKey
 import os
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
 
     __tablename__ = "reviews"
