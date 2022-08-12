adda8174d84983a53e14103a5b770e57e218ec8f
obieuan <obieuan@gmail.com>
2022-08-12 14:26:02 -0500

    update

--- models/amenity.py
+++ models/amenity.py
@@ -4,13 +4,13 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
-import os
 
 
 class Amenity(BaseModel, Base):
-    """Object representation of table amenities
     """
-    __tablename__ = "amenities"
+    Amenity Class
+    """
+    __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary=place_amenity,
                                    backref="amenities")
